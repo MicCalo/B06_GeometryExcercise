c9881d73486a3b2ffaf05d7cfb516c8afd5433ed
MichelCalonder <michel.calonder@web.de>
2023-03-31 11:32:10 +0200

    Fixed Circle

--- AddCircleShape.py
+++ AddCircleShape.py
@@ -1,19 +1,17 @@
 #This is my Circle Shape
 from painter import Painter
-import shape
+from shape import Shape
 import math
 
 
-class Circle(shape):
-    radius = 0
-    def __init__(self,x,y,color):
+class Circle(Shape):
+
+    def __init__(self,x,y,color, radius):
         super().__init__(x, y, color)
-        self.radius = abs(self.x-self.y)
+        self._radius = radius
     
     def calculate_area(self):
-        return math.pi * self.radius ** 2
+        return math.pi * self._radius ** 2
 
-    def paint(self, painter:Painter):
-        rx = self.x + self.radius 
-        ry = self.y + self.radius
-        painter.paint_ellipse(self.color, self.x, self.y, rx, ry)
+    def paint(self, painter:Painter):        
+        painter.paint_ellipse(self._color, self._x, self._y, self._radius, self._radius)
