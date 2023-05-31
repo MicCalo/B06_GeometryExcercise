1d1abbcbd50f5ef53e8a544942e58a9266250c23
MichelCalonder <michel.calonder@web.de>
2023-05-31 07:22:58 +0200

    implemented paint for triangle

--- triangle.py
+++ triangle.py
@@ -6,8 +6,18 @@ from shape import Shape
 
 class Triangle(Shape):
     def paint(self, painter: Painter):
-        painter.draw_triangle()
+        top_x = self._x
+        top_y = self._y - self._height / 3 * 2
 
+        left_x = self._x - self._base/2
+        left_y = self._y + self._height / 3
+
+        right_x = self._x + self._base/2
+        right_y = left_y
+        painter.paint_line(self._color, top_x, top_y, left_x, left_y)
+        painter.paint_line(self._color, left_x, left_y, right_x, right_y)
+        painter.paint_line(self._color,  right_x, right_y, top_x, top_y)
+        
     def calculate_area(self):
         return 0.5 * self._base * self._height
     
