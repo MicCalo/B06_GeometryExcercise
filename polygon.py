cdb5317d805520f0beca92773767b24c78654ee9
MichelCalonder <michel.calonder@web.de>
2023-05-31 07:12:55 +0200

    implemented paint and calculate_area for polygon

--- polygon.py
+++ polygon.py
@@ -2,30 +2,28 @@ from shape import Shape
 from painter import Painter
 
 class Polygon(Shape):
-    def __init__(self, x, y, color, number_of_sides):
+    def __init__(self, x, y, color, points:list=[(-10, -10), (0, -5), (10, -20), (10, 0), (5, 5), (-5, 20)]):
         super().__init__(x, y, color)
-        self.n = number_of_sides
-        self.sides = [0 for i in range(number_of_sides)]
-
-    def inputSides(self):
-        self.sides = [float(input("Seite eingeben "+str(i+1)+" : ")) for i in range(self.n)]
-
-    def dispSides(self):
-        for i in range(self.n):
-            print("Seite",i+1,"ist",self.sides[i])
+        self._points = points
 
     def calculate_area(self):
-        return 0
+       #implementation of shoelace algorithm from https://stackoverflow.com/questions/24467972/calculate-area-of-polygon-given-x-y-coordinates
+        n = len(self._points) # of corners
+        area = 0.0
+        for i in range(n):
+            j = (i + 1) % n
+            area += self._points[i][0] * self._points[j][1]
+            area -= self._points[j][0] * self._points[i][1]
+        area = abs(area) / 2.0
+        return area
 
     def paint(self, painter: Painter):
-        painter.paint_line(self._color, self._x, self._y, self._x + 100, self._y)
+        for i in range(1, len(self._points)):
+            a = self._points[i-1]
+            b = self._points[i]
+            painter.paint_line(self._color, self._x+a[0], self._y+a[1], self._x+b[0], self._y+b[1])
 
-if __name__ == "__main__":
-    polygon = Polygon(0, 0, "red", 3)
-    polygon.inputSides()
-    polygon.dispSides()
-    print("Area is", polygon.calculate_area())
-    painter = Painter()
-    polygon.paint(painter)
-
-    
+        # draw line from last to first point (make sure polygon is closed)
+        a = self._points[len(self._points)-1]
+        b = self._points[0]
+        painter.paint_line(self._color, self._x+a[0], self._y+a[1], self._x+b[0], self._y+b[1])
